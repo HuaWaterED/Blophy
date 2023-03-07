c5bde23c61acf18ea9e5319d385e5f7399909d57
HuaWaterED <1758158706@qq.com>
2023-03-07 18:28:38 +0800

    修改了选择界面的逻辑

--- Assets/Scripts/Scenes/SelectMusic/SelectMusic_ControlSpace.cs
+++ Assets/Scripts/Scenes/SelectMusic/SelectMusic_ControlSpace.cs
@@ -19,4 +19,32 @@ public class SelectMusic_ControlSpace : Public_ControlSpace
         //GlobalData.Instance. = JsonConvert.DeserializeObject<ChartData>(chart);
         GlobalData.Instance.chartData = JsonConvert.DeserializeObject<ChartData>(chart);
     }
+    Vector2 startPoint;
+    Vector2 endPoint;
+    protected override void LargeImageUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPoint = touch.position;
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endPoint = touch.position;
+                float deltaY = (endPoint - startPoint).y;
+                if (deltaY > 300 && currentElementIndex + 1 < elementCount)
+                {
+                    currentElement = allElementDistance[elementCount - 1 - ++currentElementIndex];
+                }
+                else if (deltaY < -300 && currentElementIndex - 1 >= 0)
+                {
+                    currentElement = allElementDistance[elementCount - 1 - --currentElementIndex];
+                }
+                Send();
+                StartCoroutine(Lerp());
+            }
+        }
+    }
 }
